3cf203bc8af58b267075dec2349f174be4766b87
Sam Glover <sam.glover@suffolk.edu>
2025-07-14 15:48:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# docassemble.LITLabTheme
+
+A Bootstrap theme and other visual customizations for Suffolk LIT Lab interviews.
+
+## Author
+
+Suffolk LIT Lab, litlab@suffolk.edu
---
+++ setup.py
@@ -0,0 +1,60 @@
+import os
+import sys
+from setuptools import setup, find_namespace_packages
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
+
+standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+
+def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
+    out = {}
+    stack = [(convert_path(where), '', package)]
+    while stack:
+        where, prefix, package = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                if os.path.isfile(os.path.join(fn, '__init__.py')):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                        stack.append((fn, '', new_package))
+                else:
+                    stack.append((fn, prefix + name + '/', package))
+            else:
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
+
+setup(name='docassemble.LITLabTheme',
+      version='1.0.0',
+      description=('Customizations for Suffolk LIT Lab interviews.'),
+      long_description='# docassemble.LITLabTheme\r\n\r\nA Bootstrap theme and other visual customizations for Suffolk LIT Lab interviews.\r\n\r\n## Author\r\n\r\nSuffolk LIT Lab, litlab@suffolk.edu',
+      long_description_content_type='text/markdown',
+      author='Suffolk LIT Lab',
+      author_email='litlab@suffolk.edu',
+      license='The MIT License (MIT)',
+      url='https://courtformsonline.org',
+      packages=find_namespace_packages(),
+      install_requires=['docassemble.AssemblyLine @ git+https://github.com/SuffolkLITLab/docassemble-AssemblyLine.git@main'],
+      zip_safe=False,
+      package_data=find_package_data(where='docassemble/LITLabTheme/', package='docassemble.LITLabTheme'),
+     )
+
